7639da7d4d1fe57000108f177526da25d711c58f
iprincewang <iprincewang@tencent.com>
2020-12-11 11:24:01 +0800

    初始化工程

---
+++ Game/include/BaseGame.hpp
@@ -0,0 +1,23 @@
+//
+//  BaseGame.hpp
+//  Game
+//
+//  Created by iprincewang on 2020/12/11.
+//
+
+#ifndef BaseGame_hpp
+#define BaseGame_hpp
+
+#include <iostream>
+
+class BaseGame {
+    
+private:
+    
+public:
+    void initialize();
+    void update();
+    void render();
+};
+
+#endif /* BaseGame_hpp */
---
+++ Game/src/BaseGame.cpp
@@ -0,0 +1,20 @@
+//
+//  BaseGame.cpp
+//  Game
+//
+//  Created by iprincewang on 2020/12/11.
+//
+
+#include "BaseGame.hpp"
+
+void BaseGame::initialize() {
+    
+}
+
+void BaseGame::update() {
+    
+}
+
+void BaseGame::render() {
+    
+}
